82019dd3031d6c1da867d980c3bd2b4f504ff1b8
Victor <victor@turo.com>
2023-01-12 15:43:52 +0000

    feat: add support for @semantic-release/exec

--- src/npm.ts
+++ src/npm.ts
@@ -1,13 +1,33 @@
 import config from "./";
 
+// We have a set of plugins that ideally should run after every other plugin to guarantee that things like publishing to NPM
+// already happen before these plugins run
+const pluginsThatGoAtTheEnd = new Set([
+  "@semantic-release/exec",
+  "semantic-release-fotingo",
+]);
+
+/**
+ * Given a semantic release plugin config, return the plugin name
+ * @param p Plugin config
+ * @returns Plugin name
+ */
+const getPluginName = (p: typeof config.plugins[number]) => {
+  if (typeof p === "string") {
+    return p;
+  }
+  return p[0];
+};
+
 /**
  * Semantic release configuration for NPM projects. It extends the default configuration by publishing to NPM
  * and creating a Pull Request with version updates in files like package.json
  */
 export = {
-  ...config,
   plugins: [
-    ...config.plugins,
+    ...config.plugins.filter(
+      (p) => !pluginsThatGoAtTheEnd.has(getPluginName(p))
+    ),
     [
       "@semantic-release/npm",
       {
@@ -22,5 +42,8 @@ export = {
           "ci(release): ${nextRelease.version} <% nextRelease.channel !== 'next' ? print('[skip ci]') : print('') %>\n\n${nextRelease.notes}",
       },
     ],
+    ...config.plugins.filter((p) =>
+      pluginsThatGoAtTheEnd.has(getPluginName(p))
+    ),
   ],
 };
