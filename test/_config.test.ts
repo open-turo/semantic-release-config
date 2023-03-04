9f12ab8cc1f6ed58a83c2b052113fddc5bbb9fe0
Victor <victor@turo.com>
2023-03-04 02:13:56 +0000

    test: move some tests from npm to config

---
+++ test/_config.test.ts
@@ -0,0 +1,55 @@
+import {
+  createPreset,
+  semanticReleaseGit,
+  SemanticReleasePlugin,
+} from "~/_config";
+import npm from "~/npm";
+import template = require("lodash.template");
+
+describe("config", () => {
+  describe("createPreset", () => {
+    let preset: { plugins: SemanticReleasePlugin[] };
+    beforeEach(() => {
+      preset = createPreset(["a", "b"]);
+    });
+    test("creates a preset including the default config", () => {
+      expect(preset).toMatchSnapshot();
+    });
+
+    test("@semantic-release/exec and semantic-release-fotingo are the last plugins", () => {
+      expect(preset.plugins[npm.plugins.length - 2]).toBe(
+        "semantic-release-fotingo"
+      );
+      expect(preset.plugins[npm.plugins.length - 1][0]).toBe(
+        "@semantic-release/exec"
+      );
+    });
+  });
+
+  describe("semanticReleaseGit", () => {
+    test.each(["next", undefined])(
+      "semantic-release/@git generates the right commit message for channel %s",
+      (channel) => {
+        const assets = ["a"];
+        const gitPlugin = semanticReleaseGit(assets);
+        const pluginConfig = ((gitPlugin && gitPlugin[1]) || {
+          assets: [],
+          message: "",
+        }) as {
+          assets: [];
+          message: string;
+        };
+        expect(pluginConfig.assets).toBe(assets);
+        expect(
+          template(pluginConfig.message || "")({
+            nextRelease: {
+              version: "test",
+              channel: channel,
+              notes: "This is a test release",
+            },
+          })
+        ).toMatchSnapshot();
+      }
+    );
+  });
+});
--- test/npm.test.ts
+++ test/npm.test.ts
@@ -1,36 +1,10 @@
-import template = require("lodash.template");
-
 import npm from "~/npm";
 
 describe("npm", () => {
-  test.each(["next", undefined])(
-    "semantic-release/@git generates the right commit message for channel %s",
-    (channel) => {
-      const gitPlugin = npm.plugins.find(
-        (plugin) => plugin[0] === "@semantic-release/git"
-      );
-      expect(gitPlugin).toBeDefined();
-      const pluginConfig = ((gitPlugin && gitPlugin[1]) || { message: "" }) as {
-        message: string;
-      };
-      expect(
-        template(pluginConfig.message || "")({
-          nextRelease: {
-            version: "test",
-            channel: channel,
-            notes: "This is a test release",
-          },
-        })
-      ).toMatchSnapshot();
-    }
-  );
-
-  test("@semantic-release/exec and semantic-release-fotingo are the last plugins", () => {
-    expect(npm.plugins[npm.plugins.length - 2]).toBe(
-      "semantic-release-fotingo"
-    );
-    expect(npm.plugins[npm.plugins.length - 1][0]).toBe(
-      "@semantic-release/exec"
-    );
+  test("adds npm and semantic-release/git plugins", () => {
+    const npmPlugin = npm.plugins[3];
+    const semanticReleasePlugin = npm.plugins[4];
+    expect(npmPlugin).toMatchSnapshot();
+    expect(semanticReleasePlugin).toMatchSnapshot();
   });
 });
