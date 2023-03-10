d025f448d19123b3040295d6311875f6b1285616
Steven Hurd <shurd@turo.com>
2023-03-10 18:17:51 +0000

    feat: add openapi preset configuration

--- test/gradle.test.ts
+++ test/gradle.test.ts
@@ -1,9 +1,9 @@
-import npm from "~/gradle";
+import gradle from "~/gradle";
 
 describe("gradle", () => {
   test("adds gradle-semantic-release-plugin and semantic-release/git plugins", () => {
-    const gradlePlugin = npm.plugins[3];
-    const semanticReleasePlugin = npm.plugins[4];
+    const gradlePlugin = gradle.plugins[3];
+    const semanticReleasePlugin = gradle.plugins[4];
     expect(gradlePlugin).toMatchSnapshot();
     expect(semanticReleasePlugin).toMatchSnapshot();
   });
